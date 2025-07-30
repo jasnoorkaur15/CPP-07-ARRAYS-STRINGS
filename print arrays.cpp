ee77dc2dbe4eace190d7cc2fb29b74436f6bdd16
jasnoorkaur15 <kjasnoor15@gmail.com>
2025-07-30 15:04:37 +0530

    Create print arrays.cpp

---
+++ print arrays.cpp
@@ -0,0 +1,15 @@
+//Jasnoor Kaur
+//24070123049
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[5] = {1, 2, 30, 67, 17}; // Example array
+
+    cout << "Array elements are: ";
+    for (int i = 0; i < 5; i++) {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
