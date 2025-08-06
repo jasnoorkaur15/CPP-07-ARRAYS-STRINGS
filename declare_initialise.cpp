f9ae7f72d89722499fe78aa56251d01ad9f99967
jasnoorkaur15 <kjasnoor15@gmail.com>
2025-08-06 15:23:12 +0530

    Create declare_initialise.cpp

---
+++ declare_initialise.cpp
@@ -0,0 +1,12 @@
+// Jasnoor Kaur
+// E&TC-A2
+// 24070123049
+
+#include <iostream>
+using namespace std;
+int main() {
+    char str1[] = "Hello";
+    char str2[6] = {'W','o','r','l','d','\0'};
+    cout << str1 << " " << str2;
+    return 0;
+}
